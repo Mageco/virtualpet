df79b6f4fa506047590cc452f82fb35332fef92a
Huynh Anh Tuan <mac@MACs-MacBook-Pro-3.local>
2019-08-23 12:25:52 +0700

    fix sprite

---
+++ Code/Assets/Game/Scritps/Editor/EnablePSDRemoveMatte.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+internal class EnablePSDRemoveMatte : AssetPostprocessor
+{
+	private void OnPreprocessTexture()
+	{
+		if (assetPath.Contains(".psd"))
+		{
+			TextureImporter textureImporter = (TextureImporter)assetImporter;
+
+			var serializedObject = new SerializedObject(textureImporter);
+			serializedObject.FindProperty("m_PSDRemoveMatte").boolValue = true;
+			serializedObject.FindProperty("m_PSDShowRemoveMatteOption").boolValue = true; // this is not needed unless you want to show the option (and warning)
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
