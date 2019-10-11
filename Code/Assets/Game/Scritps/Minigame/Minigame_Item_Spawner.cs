fab947dfac4d097ef0d72da82de407a1608adeac
Tuan <contact@mage.com.vn>
2019-10-11 09:01:14 +0700

    fix

---
+++ Code/Assets/Game/Scritps/Minigame/Minigame_Item_Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame_Item_Spawner : MonoBehaviour
+{ 
+    public GameObject itemPrefab;
+    GameObject item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Code/Assets/Game/Scritps/Minigame/RunObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunObject : MonoBehaviour
+{
+    public float speed = 1;
+    public bool isMove = true;
+    public float duration = 3;
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMove){
+            this.transform.position += new Vector3(speed*Time.deltaTime,0,0);
+            if(time > duration){
+                GameObject.Destroy(this.gameObject);
+            }else{
+                time += Time.deltaTime;
+            }
+
+        }
+    }
+}
