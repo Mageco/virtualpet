8040a75fe2ac6a04d3e51fd22219e43e02234699
Tuan <contact@mage.com.vn>
2019-10-29 11:59:05 +0700

    clock

---
+++ Code/Assets/Game/Scritps/Item/ClockItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockItem : MonoBehaviour
+{
+
+    public GameObject hour;
+    public GameObject minutes;
+    public GameObject second;
+    float time = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadTime();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time > 1){
+            LoadTime();
+            time = 0;
+        }else
+        {
+            time  += Time.deltaTime;
+        }
+    }
+
+    void LoadTime(){        
+        DateTime time = DateTime.Now;
+        float h = -time.Hour * 30f;
+        float m = -time.Minute * 6f;
+        float s = -time.Second * 6f;
+        hour.transform.rotation = Quaternion.Euler(new Vector3(0,0,h));
+        minutes.transform.rotation = Quaternion.Euler(new Vector3(0,0,m));
+        second.transform.rotation = Quaternion.Euler(new Vector3(0,0,s));
+    }
+}
