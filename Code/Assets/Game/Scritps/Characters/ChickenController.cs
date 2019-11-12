28ea5ec9a6a9e0848c55fc9a78ea4b7579a91f72
Tuan <contact@mage.com.vn>
2019-11-12 16:36:55 +0700

    setup minigame

---
+++ Code/Assets/Game/Scritps/Characters/ChickenController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenController : AnimalController
+{
+    bool isArrived = true;
+    public float speed = 5;
+    float maxSpeed = 20;
+    float duration = 3;
+    float time = 0;
+
+    IEnumerator MoveToPoint(Vector3 target)
+    {
+        isArrived = false;
+        while (!isArrived)
+        {
+            anim.Play("Walk");
+            anim.speed = 1 + speed/maxSpeed;
+            this.transform.position = Vector3.Lerp(this.transform.position, target, Time.deltaTime * speed);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    protected override void Think()
+    {
+        int ran = Random.Range(0, 100);
+        if (ran > 30)
+        {
+            state = AnimalState.Run;
+        }
+        else if(ran > 60)
+        {
+            state = AnimalState.Eat;
+        }
+        else
+        {
+            state = AnimalState.Idle;
+        }
+    }
+
+    
+}
