f504b50ebf717a9505917b9349576560889bd8c6
Mageco <contact@mage.com.vn>
2020-01-21 16:34:22 +0700

    Fix

---
+++ Code/Assets/Game/Scritps/UI/LanguageMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LanguageMesh : MonoBehaviour {
+
+	public TextMesh text;
+	public int dialogId = -1;
+
+    private void Awake()
+    {
+		text = this.GetComponent<TextMesh>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		ChangeLanguage ();
+	}
+
+	public void ChangeLanguage()
+	{
+        if(text != null)
+		    text.text = DataHolder.GetDialog(dialogId).GetName(MageManager.instance.GetLanguage());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
+
+
