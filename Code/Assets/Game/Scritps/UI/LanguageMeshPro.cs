f4ef17cb0e39bf6c1913721ef498179602d7fa69
Mageco <contact@mage.com.vn>
2020-04-17 11:25:42 +0700

    Add language

---
+++ Code/Assets/Game/Scritps/UI/LanguageMeshPro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class LanguageMeshPro : MonoBehaviour {
+
+	TextMeshPro text;
+	public int dialogId = -1;
+
+    private void Awake()
+    {
+		text = this.GetComponent<TextMeshPro>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		ChangeLanguage ();
+	}
+
+	public void ChangeLanguage()
+	{
+        if(text != null)
+		    text.text = DataHolder.Dialog(dialogId).GetName(MageManager.instance.GetLanguage());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
+
+
