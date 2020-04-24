e4a41e292737fa7faef5f75da849c4948e744af9
Mageco <contact@mage.com.vn>
2020-04-25 00:10:26 +0700

    Take Icon

---
+++ Code/Assets/Game/Scritps/Component/PetPhotoIcon.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetPhotoIcon : MonoBehaviour
+{
+    public GameObject[] petBasic;
+    public List<GameObject> petAccessories = new List<GameObject>();
+    int index = 0;
+
+    private void Awake()
+    {
+        petAccessories = new List<GameObject>();
+        for (int i = 0; i < petBasic.Length; i++)
+        {
+            for(int j = 0; j < 20; j++)
+            {
+                string name = petBasic[i].name + "_Accessory_" + (j + 1).ToString();
+                GameObject go = GameObject.Find(name);
+                if (go != null)
+                {
+                    petAccessories.Add(go);
+                    //go.SetActive(false);
+                    //Debug.Log(go.name);
+                }
+            }
+        }
+
+        Debug.Log(petAccessories[0].name);
+    }
+
+    public bool NextPet()
+    {
+        //Debug.Log(index);
+        //Debug.Log(petAccessories[0].name);
+        if (index >= petAccessories.Count)
+            return false;
+        for(int i = 0; i < petAccessories.Count; i++)
+        {
+            if (i == index)
+                petAccessories[i].SetActive(true);
+            else
+                petAccessories[i].SetActive(false);
+        }
+        index++;
+
+        return true;
+    }
+}
---
+++ Code/Assets/Game/Scritps/Editor/Inspectors/PetPhotoIconInspector.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PetPhotoIcon))]
+public class PetPhotoIconInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Undo.RecordObject(target, "Changed");
+
+        if (GUILayout.Button("Add Pet", GUILayout.Width(200)))
+        {
+            ((PetPhotoIcon)target).petBasic = ArrayHelper.Add(null, ((PetPhotoIcon)target).petBasic);
+        }
+
+        for (int i=0;i< ((PetPhotoIcon)target).petBasic.Length; i++)
+        {
+            GUILayout.BeginHorizontal();
+            ((PetPhotoIcon)target).petBasic[i] = (GameObject)EditorGUILayout.ObjectField("pet " + (i+1).ToString(), ((PetPhotoIcon)target).petBasic[i], typeof(GameObject), false, GUILayout.Width(300));
+            if (GUILayout.Button("x", GUILayout.Width(20)))
+            {
+                ((PetPhotoIcon)target).petBasic = ArrayHelper.Remove(i, ((PetPhotoIcon)target).petBasic);
+            }
+            GUILayout.EndHorizontal();
+        }
+        if (GUILayout.Button("Load Accessory", GUILayout.Width(200)))
+        {
+            for(int i=0;i< ((PetPhotoIcon)target).petBasic.Length; i++)
+            {
+                var sourcePath = AssetDatabase.GetAssetPath(((PetPhotoIcon)target).petBasic[i]);
+                sourcePath = sourcePath.Replace(".prefab", "");
+                for (int n = 0; n < 20; n++)
+                {
+                    var copyPath = sourcePath + "_Accessory_" + (n + 1).ToString() + ".prefab";
+                    Debug.Log(copyPath);
+                    if (GameObject.Find(((PetPhotoIcon)target).petBasic[i].name + "_Accessory_" + (n + 1).ToString()))
+                        DestroyImmediate(GameObject.Find(((PetPhotoIcon)target).petBasic[i].name + "_Accessory_" + (n + 1).ToString()));
+                    GameObject sourceObject = AssetDatabase.LoadAssetAtPath<GameObject>(copyPath);
+                    if(sourceObject != null)
+                    {
+                        GameObject copyObject = Instantiate(sourceObject) as GameObject;
+                        copyObject.name = ((PetPhotoIcon)target).petBasic[i].name + "_Accessory_" + (n + 1).ToString();
+                    }
+                }
+            }
+        }
+    }
+}
