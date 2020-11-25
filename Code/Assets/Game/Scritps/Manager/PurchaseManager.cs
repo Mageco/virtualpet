7f7c8709c4095f45ff212c6658daa697534886de
Mageco <contact@mage.com.vn>
2020-11-25 13:49:31 +0700

    Fix

--- Code/Assets/Game/Scritps/Manager/PurchaseManager.cs
+++ Code/Assets/Game/Scritps/Manager/PurchaseManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Purchasing;
@@ -439,3 +440,4 @@ public class PurchaseManager : MonoBehaviour, IStoreListener
 	#endregion
 }
 
+*/
