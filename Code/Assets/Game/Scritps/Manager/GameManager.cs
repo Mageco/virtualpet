84e86a0bd91989cb7e5c8f7d2800b9244460127a
Mageco <contact@mage.com.vn>
2020-11-26 12:31:36 +0700

    Update GameManager.cs

--- Code/Assets/Game/Scritps/Manager/GameManager.cs
+++ Code/Assets/Game/Scritps/Manager/GameManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Mage.Models.Attributes;
 using Mage.Models.Users;
@@ -1617,7 +1616,7 @@ public class GameManager : MonoBehaviour
                 {
                     isCollect = true;
                 }
-            }catch(Exception e){
+            }catch(System.Exception e){
                 isCollect = false;
             }
         }
